94b573293d047e38b04570e38f7971686db4aa2e
Samuel Espinosa <saespmar@gmail.com>
2019-06-26 14:41:11 +0200

    Create registerUser operation in PublicActions

--- src/main/java/com/saespmar/storeManager/service/PublicActions.java
+++ src/main/java/com/saespmar/storeManager/service/PublicActions.java
@@ -3,6 +3,9 @@ package com.saespmar.storeManager.service;
 import com.saespmar.storeManager.dto.*;
 import com.saespmar.storeManager.model.*;
 import com.saespmar.storeManager.operations.*;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -39,7 +42,7 @@ public class PublicActions {
         }
         return productsDTO;
     }
-
+    
     public static ProductDTO getProduct(int id){
         if (id < 1) return null;
         Product p = productOps.readProduct(id);
@@ -56,8 +59,8 @@ public class PublicActions {
         
         return pdto;
     }
-
-	public static Set<OpinionDTO> getOpinions(int id){
+    
+    public static Set<OpinionDTO> getOpinions(int id){
         if (id < 1) return null;
         Product p = productOps.readProduct(id);
         ProductDTO pdto = new ProductDTO();
@@ -88,4 +91,72 @@ public class PublicActions {
         
         return opinionsDTO;
     }
+    
+    public static CustomerDTO registerUser(String email, String password){
+        if (email == null || password == null) return null;
+        if (!validMail(email)) return null;
+        if (password.length() < 8 || password.length() > 30) return null;
+        
+        // Check if the password has an uppercase letter, a lowercase letter and a number
+        boolean up = false, low = false, num = false;
+        for (int i = 0; i < password.length() && !securePassword(up, low, num); i++){
+            char current = password.charAt(i);
+            if (!up && Character.isUpperCase(current)) up = true;
+            else if (!low && Character.isLowerCase(current)) low = true;
+            else if (!num && Character.isDigit(current)) num = true;
+        }
+        if (!securePassword(up, low, num)) return null;
+        
+        // Hash the password
+        String hashedPassword = hashPassword(password);
+        
+        // Add the customer to the database
+        int id = customerOps.createCustomer(new Customer(email, hashedPassword));
+        
+        if (id == 0) return null;
+        else {
+            CustomerDTO cdto = new CustomerDTO();
+            cdto.setEmail(email);
+            cdto.setId(id);
+            return cdto;
+        }
+    }
+    
+    private static boolean validMail(String email){
+        if (email.length() > 50) return false;
+        if (email.indexOf('@') == -1) return false;
+        String name = email.substring(0, email.indexOf('@')); // The string after the @
+        String domain = email.substring(email.indexOf('@')+1, email.length());
+        if (domain.indexOf('.') == -1) return false;
+        String server = domain.substring(0, domain.indexOf('.')); // The string between the @ and the .
+        String tld = domain.substring(domain.indexOf('.'), domain.length()); // The string after the .
+        return name.length() > 0 && server.length() > 0 && tld.length() > 0;
+    }
+    
+    private static boolean securePassword(boolean uppercase, boolean lowercase, boolean number){
+        return uppercase && lowercase && number;
+    }
+    
+    private static String hashPassword(String password)
+    {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-512");
+            byte[] messageDigest = md.digest(password.getBytes());
+            
+            // Convert into hex value
+            BigInteger signum = new BigInteger(1, messageDigest);
+            String hashedPassword = signum.toString(16);
+            
+            // If the hash isn't 32 bit, add zeros at the beginning
+            while (hashedPassword.length() < 32)
+                hashedPassword = "0" + hashedPassword;
+            
+            return hashedPassword;
+        }
+        
+        catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    
 }
