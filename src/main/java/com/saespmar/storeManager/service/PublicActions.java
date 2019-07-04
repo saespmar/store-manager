c57719187caea2ac7efac4019aadcfb3bdae2d4c
Samuel Espinosa <saespmar@gmail.com>
2019-07-04 14:57:33 +0200

    Add custom exceptions in PublicActions

--- src/main/java/com/saespmar/storeManager/service/PublicActions.java
+++ src/main/java/com/saespmar/storeManager/service/PublicActions.java
@@ -1,6 +1,7 @@
 package com.saespmar.storeManager.service;
 
 import com.saespmar.storeManager.dto.*;
+import com.saespmar.storeManager.exception.*;
 import com.saespmar.storeManager.model.*;
 import com.saespmar.storeManager.operations.*;
 import java.util.HashSet;
@@ -17,12 +18,13 @@ public class PublicActions {
     static UserOrderOps userOrderOps = new UserOrderOpsImpl();
     
     
-    public static Set<ProductDTO> getProductsFromCategory(int id) {
+    public static Set<ProductDTO> getProductsFromCategory(int id) throws CategoryNotFoundException {
         // Check input values
-        if (id < 1) return null;
+        if (id < 0) throw new IllegalArgumentException("ID must be positive");
+        Category category = categoryOps.readCategory(id);
+        if (category == null) throw new CategoryNotFoundException("Category with ID " + id + " not found");
         
         // Transform all the Product models into DTO 
-        Category category = categoryOps.readCategory(id);
         Set<Product> products = category.getProducts();
         Set<ProductDTO> productsDTO = new HashSet<>();
         for (Product p : products) {
@@ -31,20 +33,22 @@ public class PublicActions {
         return productsDTO;
     }
     
-    public static ProductDTO getProduct(int id){
+    public static ProductDTO getProduct(int id) throws ProductNotFoundException{
         // Check input values
-        if (id < 1) return null;
+        if (id < 0) throw new IllegalArgumentException("ID must be positive");
         
-        Product p = productOps.readProduct(id);        
+        Product p = productOps.readProduct(id); 
+        if (p == null) throw new ProductNotFoundException("Product with ID " + id + " not found");
         return ServiceUtils.productTransform(p);
     }
     
-    public static Set<OpinionDTO> getOpinions(int id){
+    public static Set<OpinionDTO> getOpinions(int id) throws ProductNotFoundException{
         // Check input values
-        if (id < 1) return null;
+        if (id < 0) throw new IllegalArgumentException("ID must be positive");
         
         // All the opinions need an associated ProductDTO
         Product p = productOps.readProduct(id);
+        if (p == null) throw new ProductNotFoundException("Product with ID " + id + " not found");
         ProductDTO pdto = ServiceUtils.productTransform(p);
         
         // Transform models into DTO 
@@ -57,11 +61,11 @@ public class PublicActions {
         return opinionsDTO;
     }
     
-    public static CustomerDTO registerUser(String email, String password){
+    public static CustomerDTO registerUser(String email, String password) throws DuplicatedCustomerException{
         // Check input values
-        if (email == null || password == null) return null;
-        if (!validMail(email)) return null;
-        if (password.length() < 8 || password.length() > 30) return null;
+        if (email == null || password == null) throw new IllegalArgumentException("Parameters can't be null");
+        if (!validMail(email)) throw new IllegalArgumentException("Invalid email");
+        if (password.length() < 8 || password.length() > 30) throw new IllegalArgumentException("Incorrect password length");
         
         // Check if the password has an uppercase letter, a lowercase letter and a number
         boolean up = false, low = false, num = false;
@@ -71,33 +75,35 @@ public class PublicActions {
             else if (!low && Character.isLowerCase(current)) low = true;
             else if (!num && Character.isDigit(current)) num = true;
         }
-        if (!ServiceUtils.securePassword(up, low, num)) return null;
+        if (!ServiceUtils.securePassword(up, low, num)) 
+            throw new IllegalArgumentException("Password must have an uppercase letter, a lowercase letter and a number");
         
         // Hash the password
         String hashedPassword = ServiceUtils.hashPassword(password);
         
         // Add the customer to the database
-        if (customerOps.searchCustomer(email) == null){
+        Customer c = customerOps.searchCustomer(email);
+        if (c == null){
             int id = customerOps.createCustomer(new Customer(email, hashedPassword));
             CustomerDTO cdto = new CustomerDTO();
             cdto.setEmail(email);
             cdto.setId(id);
             return cdto;
         } else {
-            return null;
+            throw new DuplicatedCustomerException("Customer with email " + email + " is already in the database, with ID " + c.getId());
         }
     }
     
-    public static CustomerDTO logIn(String email, String password){
+    public static CustomerDTO logIn(String email, String password) throws CustomerNotFoundException{
         // Check input values
-        if (email == null || password == null) return null;
+        if (email == null || password == null) throw new IllegalArgumentException("Parameters can't be null");
         
         // Check if the customer exists and the password is correct
         Customer c = customerOps.searchCustomer(email);
         if (c != null && c.getPassword().equals(ServiceUtils.hashPassword(password))){
             return ServiceUtils.customerTransform(c);
         } else {
-            return null;
+            throw new CustomerNotFoundException("Customer with email " + email + " not found");
         }
     }
     
